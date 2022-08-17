476503fcb2d8f4444cbd3aef8f3d91ff08d50c52
Tafsiru Loum <tafloum33@gmail.com>
2022-08-17 12:00:49 +0000

    All switch themes are working fine.

--- calApp.js
+++ calApp.js
@@ -5,12 +5,9 @@ const reset = document.querySelector('.reset');
 
 // Changing theme colors
 
-const themes = document.querySelectorAll('.themes');
-const themeOneColor = document.querySelector('#theme-one');
-const themeTwoColor = document.querySelector('#theme-two');
-const themeThreeColor = document.querySelector('#theme-three');
-
-const bodyEliBackgroundColor = document.querySelector('body');
+const bodyEliBackgroundColor = document.querySelector('.body');
+const themes = document.querySelector('.switch-button');
+const themeToggler = document.querySelector('#themes');
 const calculatorTitle = document.querySelector('.cal-title');
 const displayOuput = document.querySelector('.display');
 const inputSection = document.querySelector('.input');
@@ -26,90 +23,101 @@ input.forEach(key=>{
 
 function calculate(){
     let buttonText = this.innerText;
-    let errorMessage = "./+-*=";
-   
     if(buttonText == 'RESET'){
         output.textContent = '';
-        return;
-        
+        return;   
     }
-    
     if(buttonText == "DEL"){
         output.textContent = output.textContent.substr(0,output.textContent.length-1);
         return;
     }
-
     if(buttonText == "="){
         output.innerText = eval(output.innerText);
-        output.style.animation = "big 0.5s ease-in-out";
-        
+        output.style.animation = "big 0.5s ease-in-out";  
     }
-    
     else{
         output.textContent += buttonText;
         return;
     }
-
-    
 }
 
 
 
+// Theme Toggler Function
 
-// Calculator Themes Function
-
-
-function themeToggler(){
-    if (themes.value == '1') {
-        bodyEliBackgroundColor.classList.remove(".themeThreeBody");
-        bodyEliBackgroundColor.classList.add(".themeOneBody");
-        calculatorTitle.classList.remove("theme3CalTitle");
-        calculatorTitle.classList.add("theme1CalTitle");
-        displayOuput.classList.remove(".themeThreeDisplay");
-        displayOuput.classList.add(".themeOneDisplay");
-        inputSection.classList.remove(".themeThreeInputColor");
-        inputSection.classList.add(".themeOneInputColor");
-        inputButton.classList.remove(".themeThreeButSha");
-        inputButton.classList.add(".themeOneButSha");
-        clearButton.classList.remove(".themeThreeClearButton");
-        clearButton.classList.add(".themeOneClearButton");
-        equalButton.classList.remove(".themeThreeEqual");
-        equalButton.classList.add(".themeOneEqual");
-        document.querySelector('#theme2').style.display = 'none';
-        document.querySelector('#theme3').style.display = 'none';
-    } else if (themes.value == '2') {
-        bodyEliBackgroundColor.classList.remove(".themeOneBody");
-        bodyEliBackgroundColor.classList.add(".themeTwoBody")
+themes.addEventListener('click', () => {
+    
+    if(themeToggler == document.querySelector('.themeOne-toggler')){
+        themeToggler.classList.remove('themeOne-toggler');
+        themeToggler.classList.add('themeTwo-toggler');
+        bodyEliBackgroundColor.classList.remove("themeOneBody");
+        bodyEliBackgroundColor.classList.add("themeTwoBody");
         calculatorTitle.classList.remove("theme1CalTitle");
         calculatorTitle.classList.add("theme2CalTitle");
-        displayOuput.classList.remove(".themeOneDisplay");
-        displayOuput.classList.add(".themeTwoDisplay");
-        inputSection.classList.remove(".themeOneInputColor");
-        inputSection.classList.add(".themeTwoInputColor");
-        inputButton.classList.remove(".themeOneButSha");
-        inputButton.classList.add(".themeTwoButSha");
-        clearButton.classList.remove(".themeOneClearButton");
-        clearButton.classList.add(".themeTwoClearButton");
-        equalButton.classList.remove(".themeOneEqual");
-        equalButton.classList.add(".themeTwoEqual");
-        document.querySelector('#theme1').style.display = 'none';
-        document.querySelector('#theme3').style.display = 'none';
-    } else if (themes.value == '3'){
-        bodyEliBackgroundColor.classList.remove(".themeTwoBody");
-        bodyEliBackgroundColor.classList.add(".themeThreeBody");
+        displayOuput.classList.remove("themeOneDisplay");
+        displayOuput.classList.add("themeTwoDisplay");
+        inputSection.classList.remove("themeOneInputColor");
+        inputSection.classList.add("themeTwoInputColor");
+        equalButton.classList.remove("themeOneEqual");
+        equalButton.classList.add("themeTwoEqual");
+        clearButton.forEach(key =>{
+            key.classList.remove("themeOneClearButton");
+            key.classList.add("themeTwoClearButton");  
+        })
+        inputButton.forEach(key =>{
+            key.classList.remove("themeOneButSha");
+            key.classList.add("themeTwoButSha");
+        })
+        
+    }else if(themeToggler == document.querySelector('.themeTwo-toggler')){
+        themeToggler.classList.remove('themeTwo-toggler');
+        themeToggler.classList.add('themeThree-toggler');
+        bodyEliBackgroundColor.classList.remove("themeTwoBody");
+        bodyEliBackgroundColor.classList.add("themeThreeBody");
         calculatorTitle.classList.remove("theme2CalTitle");
         calculatorTitle.classList.add("theme3CalTitle");
-        displayOuput.classList.remove(".themeTwoDisplay");
-        displayOuput.classList.add(".themeThreeDisplay");
-        inputSection.classList.remove(".themeTwoInputColor");
-        inputSection.classList.add(".themeThreeInputColor");
-        inputButton.classList.remove(".themeTwoButSha");
-        inputButton.classList.add(".themeThreeButSha");
-        clearButton.classList.remove(".themeTwoClearButton");
-        clearButton.classList.add(".themeThreeClearButton");
-        equalButton.classList.remove(".themeTwoEqual");
-        equalButton.classList.add(".themeThreeEqual");
-        document.querySelector('#theme1').style.display = 'none';
-        document.querySelector('#theme2').style.display = 'none';
+        displayOuput.classList.remove("themeTwoDisplay");
+        displayOuput.classList.add("themeThreeDisplay");
+        inputSection.classList.remove("themeTwoInputColor");
+        inputSection.classList.add("themeThreeInputColor");
+        equalButton.classList.remove("themeTwoEqual");
+        equalButton.classList.add("themeThreeEqual");
+        clearButton.forEach(key =>{
+            key.classList.remove("themeTwoClearButton");
+            key.classList.add("themeThreeClearButton");  
+        })
+        inputButton.forEach(key =>{
+            key.classList.remove("themeTwoButSha");
+            key.classList.add("themeThreeButSha");
+        })
+    }else if(themeToggler == document.querySelector('.themeThree-toggler')){
+        themeToggler.classList.remove('themeThree-toggler');
+        themeToggler.classList.add('themeOne-toggler');
+        bodyEliBackgroundColor.classList.remove("themeThreeBody");
+        bodyEliBackgroundColor.classList.add("themeOneBody");
+        calculatorTitle.classList.remove("theme3CalTitle");
+        calculatorTitle.classList.add("theme1CalTitle");
+        displayOuput.classList.remove("themeThreeDisplay");
+        displayOuput.classList.add("themeOneDisplay");
+        inputSection.classList.remove("themeThreeInputColor");
+        inputSection.classList.add("themeOneInputColor");
+        equalButton.classList.remove("themeThreeEqual");
+        equalButton.classList.add("themeOneEqual");
+        clearButton.forEach(key =>{
+            key.classList.remove("themeThreeClearButton");
+            key.classList.add("themeOneClearButton");  
+        })
+        inputButton.forEach(key =>{
+            key.classList.remove("themeThreeButSha");
+            key.classList.add("themeOneButSha");
+        })
     }
-}
+    
+})
+
+
+
+
+
+
+
